986a290f44d430808e20d05545d8954e5029f761
Alistair Willis <alistairrwillis@gmail.com>
2017-06-11 22:53:08 +0900

    bug fix

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,9 @@ const AuthController = require('../controllers/auth-controller');
 const {authenticate} = require('../middleware/authenticate');
 
 module.exports = (app) => {
-  app.get('/home', (req, res) => {
-    res.redirect('/');
-  })
+  // app.get('/home', (req, res) => {
+  //   res.redirect('/');
+  // })
 
   app.get('/api/data/current', authenticate, DataController.getTopicsByUser);
   app.get('/api/data', DataController.getTopicIndex);
@@ -18,8 +18,8 @@ module.exports = (app) => {
   app.post('/api/auth/login', AuthController.login);
   app.delete('/api/auth/logout', authenticate, AuthController.logout);
 
-  app.get('*', (req, res) => {
-    res.redirect('/');
+  // app.get('*', (req, res) => {
+  //   res.redirect('/');
     // res.status(404).send({message: 'Page not found'});
   })
 
