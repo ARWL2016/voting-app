6752000b1d209ae2c12c09ad2f8adfce700604aa
Alistair Willis <alistairrwillis@gmail.com>
2017-06-11 22:59:04 +0900

    bug fix

--- api/config/index.js
+++ api/config/index.js
@@ -1,13 +1,13 @@
 // module.exports = require('./development.json');
 
-let env = process.env.NODE_ENV || 'development';
-console.log('ENV: ', env);
+// let env = process.env.NODE_ENV || 'development';
+// console.log('ENV: ', env);
 
-if (env === 'development') {
-  // const config = require('./development.json');
-  console.log(config);
-  process.env.JWT_SECRET = config.JWT_SECRET;
-  process.env.MONGO_URI = config.MONGO_URI;
-  console.log(process.env.MONGO_URI);
-}
+// if (env === 'development') {
+//   // const config = require('./development.json');
+//   console.log(config);
+//   process.env.JWT_SECRET = config.JWT_SECRET;
+//   process.env.MONGO_URI = config.MONGO_URI;
+//   console.log(process.env.MONGO_URI);
+// }
 
